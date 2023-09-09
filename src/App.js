7747bce9d68a93df1f0f0806fcc96b9f9652f485
torinit-darshan <darshan.sudake@torinit.ca>
2023-09-09 19:49:26 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h4>Hi There!</h4>
+        <h4>Hi there!</h4>
         <h1 id="head-text">{process.env.REACT_APP_HEAD_TEXT}</h1>
       </header>
     </div>
